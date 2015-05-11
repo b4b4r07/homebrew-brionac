ba52d151db2eb709bb33563818930604818c8ae5
b4b4r07 <b4b4r07@gmail.com>
2015-05-12 00:07:13 +0900

    Add sha1

--- brionac.rb
+++ brionac.rb
@@ -5,12 +5,12 @@ class Brionac < Formula
     version '0.1.0'
 
     if Hardware.is_64_bit?
-        url "https://github.com/b4b4r07/gomi/releases/download/v0.1.0/brionac_darwin_amd64"
-        sha1 ""
+        url "https://github.com/b4b4r07/brionac/releases/download/v0.1.0/brionac_darwin_amd64"
+        sha1 "6e9a767cccc485ef987146fd3dd32abff95c8370"
         KIND="amd64"
     else
-        url "https://github.com/b4b4r07/gomi/releases/download/v0.1.0/brionac_darwin_386"
-        sha1 ""
+        url "https://github.com/b4b4r07/brionac/releases/download/v0.1.0/brionac_darwin_386"
+        sha1 "c551a95a872126085450ce3e55f3dff49cf75321"
         KIND="386"
     end
 
